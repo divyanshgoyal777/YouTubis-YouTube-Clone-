178a38ce79bf743d0a7d573cb02bd2f0ad669c7f
Divyansh Goyal <147051621+divyanshgoyal777@users.noreply.github.com>
2024-07-05 18:18:25 +0530

    Update keys.js

--- frontend/src/Api/keys.js
+++ frontend/src/Api/keys.js
@@ -1,14 +1,5 @@
 const apiKeys = [
-    'AIzaSyB4vkLWoxkEiW_rgwVUqGQePLp_FWZN5Fw',
-    'AIzaSyDnK7tG7m9Uoq8oLdpJSFhQDfDT4gZK_Ys',
-    'AIzaSyB6K_RXjCsghO8seGZjJI5CWaVI4RXOQOA',
-    'AIzaSyCeFBNK2n9yaNphbdJYZlr6WAFUNGdcSYE',
-    'AIzaSyAyUWbVxwNdJqwpBYwq0Y3A0Go7BqpY614',
-    'AIzaSyDnd6Nam7F7FmwK0DdVr6orja7WQb8W8CA',
-    'AIzaSyAB9UIXlsZjpstkjKsiCsBdiffvdaGsue4',
-    'AIzaSyDRDwbEp93hu-gZGSeOUByNRhUdPDV20Z4',
-    'AIzaSyATOpGrHzIMcH6qzBGqMPQ7ViMzvF-Pttc',
-    'AIzaSyA9Ahxx6P_p9stja4PwuTt44a8a4FKxmMk'
+    // Enter Your keys
 ]
 
 export default apiKeys;
